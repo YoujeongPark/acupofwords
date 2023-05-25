674254f663d3712b99eac69a4e3faa349ffe95a7
Youjeong Park <pyj0695@gmail.com>
2023-05-25 19:04:14 +0900

    feat : home, memory, write pages

---
+++ app/memory/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+import React, {useState, useEffect} from 'react'
+import {Question} from "@/components/Question";
+import {Answer} from "@/components/Answer";
+import {EvaluateToday} from "@/components/EvaluateToday";
+
+export default function memory() {
+
+
+
+  return (
+    <div className = "oveflow-y">
+      <Question/>
+      <div className = "flex-row">
+        <div style = {{width : '80%'}}>
+          <Answer time = {'아침'} />
+          <Answer time = {'저녁'} />
+          <EvaluateToday/>     
+        </div>
+        <div>
+          일러스트
+        </div>
+      </div>
+    </div>
+   
+  )
+}
+
---
+++ app/write/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+export default function write() {
+  return (
+   <div>  
+    <div>
+      
+    </div>
+
+
+   </div>
+  )
+}
