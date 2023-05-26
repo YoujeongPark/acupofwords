22b23be7c7fd23b0697ef22dd46dd25fe8a48bba
Youjeong Park <pyj0695@gmail.com>
2023-05-26 21:01:38 +0900

    fix : change word

--- components/EmotionButton.tsx
+++ components/EmotionButton.tsx
@@ -8,12 +8,12 @@ interface ButtonProps {
 }
 const EmotionButton: React.FC<ButtonProps> = ({ onClick, children , type }) => {
 
-  const expression : any = {happy : '행복',peaceful : '평온', sad : '슬픔', angry : '화남',worry : '걱정'}
+  const mood : any = {happy : '행복',peaceful : '평온', sad : '슬픔', angry : '화남',worry : '걱정'}
 
   return (
     <button onClick={onClick} className="flex-row items-center gap-5 mt-3">
       <Image src={`/${type}.svg`} alt={type} width={30} height={30}/>
-      {expression[type]}
+      {mood[type]}
     </button>
   );
 };
