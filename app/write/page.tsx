3c80a2dd74abed65326db1303174500b648b5b61
Youjeong Park <pyj0695@gmail.com>
2023-05-26 20:29:25 +0900

    feat : write, memory page

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,64 +1,27 @@
 'use client';
-import Image from 'next/image';
-import Memory from "@/components/Memory";
-import EmotionButton from "@/components/EmotionButton";
-import StartMemory from "@/components/StartMemory";
+import React, {useState, useEffect} from 'react'
+import {Question} from "@/components/Question";
+import {Answer} from "@/components/Answer";
+import {EvaluateToday} from "@/components/EvaluateToday";
 
-export default function write() {
-
-  const left = () => {
-
-  }
-
-  const right = () => {
-
-  }
-
-  const startWrite = () => {
-    console.log('startWrite')
-  }
-
-  const expression = ['happy', 'peaceful', 'sad', 'angry', 'worry']
+export default function memory() {
 
 
-  const filter = () => {
-
-  }
 
   return (
-    <div>
-      <div className="flex-row-center">
-        <Image src="/left.svg" className= "cursor" alt="left" width={30} height={30} onClick={left} />
-        <div>2023년 5월</div>
-        <Image src="/right.svg" className= "cursor" alt="right" width={30} height={30} onClick={right} />
-      </div>
-      <div className="flex-row">
-        <div className = "left-box oveflow-y">
-          <div>즐겨찾기</div>
-          <div className = "memory-grid">
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {true}> </Memory>
-          </div>
-          <div className="divide" />
-          <div className = "memory-grid">
-            <StartMemory onClick={startWrite} > </StartMemory>
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {false}> </Memory>
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {false}> </Memory>
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {false}> </Memory>
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {false}> </Memory>
-            <Memory onClick={startWrite} emotion={"emotion"} isFavority = {false}> </Memory>
-          </div>
+    <div className = "oveflow-y">
+      <Question/>
+      <div className = "flex-row">
+        <div style = {{width : '80%'}}>
+          <Answer time = {'아침'} />
+          <Answer time = {'저녁'} />
+          <EvaluateToday/>     
         </div>
-        <div className = "divide-90" />
         <div>
-          <input type="search" name="search" placeholder="   검색" rel="search" />
-          <div className="mood mt-5">
-            <div>그날의 무드</div>
-            {expression.map((item: any, i: any) => (
-              <EmotionButton key={i} type={item} children={item} onClick={filter} />
-            ))}
-          </div>
+          일러스트
         </div>
       </div>
-    </div>
+    </div>   
   )
 }
+
