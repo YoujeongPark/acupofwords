0660c19e543535743b21e8ea80570e7b347be506
Youjeong Park <pyj0695@gmail.com>
2023-05-27 09:59:25 +0900

    fix : gif rendering

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -8,23 +8,29 @@ import Image from 'next/image';
 export default function memory() {
 
 
-  const [themeMode, setThemeMode] = useState<string>((new Date().getHours() > 4 && new Date().getHours() < 17) ? 'day' : 'night');
+  const [themeMode, setThemeMode] = useState<string>('');
 
   useEffect(() => {
-    document.body.dataset.theme = themeMode
-  }, [themeMode])
+    if (new Date().getHours() > 4 && new Date().getHours() < 17) {
+      setThemeMode('day')
+      document.body.dataset.theme = 'day'
+    } else {
+      setThemeMode('night')
+      document.body.dataset.theme = 'night'
+    }
+  }, [])
 
   return (
     <div className="oveflow-y">
       <Question />
       <div className="flex-row">
         <div style={{ width: '80%' }}>
-          <Answer disable = {false} time={'아침'} write = {'| 질문의 답을 적어주세요. '} themeMode = {themeMode}/>
-          <Answer disable = {false} time={'저녁'} write = {'| 고민 후 저녁에 다시 답을 적어주세요. '} themeMode = {themeMode}/>
-          <EvaluateToday themeMode = {themeMode}/>
+          <Answer disable={false} time={'아침'} write={'| 질문의 답을 적어주세요. '} themeMode={themeMode} />
+          <Answer disable={false} time={'저녁'} write={'| 고민 후 저녁에 다시 답을 적어주세요. '} themeMode={themeMode} />
+          <EvaluateToday themeMode={themeMode} />
         </div>
-        <div className = "ml-20">
-          <Image src={`/${themeMode}.gif`} className= "cursor" alt="left" width = {400} height = {0} />
+        <div className="ml-20">
+          <Image src={`/${themeMode}.gif`} className="cursor" alt="left" width={400} height={0} />
         </div>
       </div>
     </div>
