82066fda9327e829c89ceb14fd08fae02282f917
Youjeong Park <pyj0695@gmail.com>
2023-05-28 10:51:24 +0900

    fix : add blue css and error

--- components/Question.tsx
+++ components/Question.tsx
@@ -17,7 +17,7 @@ export const Question = () => {
 
   useEffect(() => {
     setQuestion(getData[rand(0, getData.length)].answer? getData[rand(0, getData.length)].answer : '');
-  },[])
+  },[getData])
 
   function rand(min : number, max : number) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
