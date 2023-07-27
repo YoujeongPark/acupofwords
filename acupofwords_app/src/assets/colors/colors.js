93ec74dbc4c2c0995acbfd86947ebe041c4e34ed
Youjeong Park <pyj0695@gmail.com>
2023-07-27 21:00:52 +0900

    add : design system colors

--- acupofwords_app/src/assets/colors/colors.js
+++ acupofwords_app/src/assets/colors/colors.js
@@ -1,12 +1,12 @@
 const colors = {
-    background: '#F9F9FB',
-    textDark: '#313234',
-    primary: '#F5CA48',
-    secondary: '#F26C68',
-    textLight: '#CDCDCD',
-    price: '#E4723C',
-    white: '#FFF',
-    black: '#000',
+    darkGrey: '#454544',
+    grey: '#454544',
+    nightBG: '#DEE8F1',
+    dayBG: '#F4E8C7',
+    white: '#FAF9F6',
+    whiteTransparent: '#FAF9F6',
+    lightGrey: '#E3DDD3',
+    dayBG: '#F4E5D1',
   };
   
   export default colors;
